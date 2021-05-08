0efe0be012ac1e887910ae6d1a202cfba1addb50
Vineet thutheja <vthutheja@gmail.com>
2021-05-08 22:49:34 +0530

    Brand Api Fix

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UnitController;
+use App\Http\Controllers\BrandController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
