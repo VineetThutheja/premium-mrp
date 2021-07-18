8ea8145a90c5e091551bba4c9668df42bdb30adb
Vineet thutheja <vthutheja@gmail.com>
2021-07-18 19:13:06 +0530

    Order Resource

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $order = parent::toArray($request);
+        $order["businessName"] = $this->contact->buisnessName;
+
+        return $order;
+    }
+}
